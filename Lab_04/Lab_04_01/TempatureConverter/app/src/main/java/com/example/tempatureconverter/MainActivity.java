26b5d9d7fc7ef5f59672a3c0602c19c9ee92e898
tienbinh4060 <tienbinhnguyen0246@gmail.com>
2025-06-05 23:38:59 +0700

    Them Lab_04 voi Lab_04_01 TempatureConverter va Lab_04_02 BodyMassIndex

---
+++ Lab_04/Lab_04_01/TempatureConverter/app/src/main/java/com/example/tempatureconverter/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.tempatureconverter;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtFar, edtCel;
+    Button btnCF, btnFC, btnClear;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtFar = findViewById(R.id.edt_fahrenheit);
+        edtCel = findViewById(R.id.edt_celsius);
+        btnCF = findViewById(R.id.btn_cf);
+        btnFC = findViewById(R.id.btn_fc);
+        btnClear = findViewById(R.id.btn_clear);
+
+        btnCF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String cel = edtCel.getText()+"";
+                int C = Integer.parseInt(cel);
+                edtFar.setText(""+dcf.format(C*1.8+32));
+            }
+        });
+        btnFC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String cel = edtCel.getText()+"";
+                int C = Integer.parseInt(cel);
+                edtFar.setText(""+dcf.format(C*1.8+32));
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtFar.setText("");
+                edtCel.setText("");
+            }
+        });
+    }
+}

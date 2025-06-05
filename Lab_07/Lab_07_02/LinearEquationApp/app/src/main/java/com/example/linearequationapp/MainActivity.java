d2b972c5dc945dffa01771ea939ff86a8cd37afe
tienbinh4060 <tienbinhnguyen0246@gmail.com>
2025-06-06 02:06:28 +0700

    Them Lab_07

---
+++ Lab_07/Lab_07_02/LinearEquationApp/app/src/main/java/com/example/linearequationapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.linearequationapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtA, edtB;
+    Button btnResult;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edtA = findViewById(R.id.edt_a);
+        edtB = findViewById(R.id.edt_b);
+        btnResult = findViewById(R.id.btn_result);
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                int a = Integer.parseInt(edtA.getText().toString());
+                int b = Integer.parseInt(edtB.getText().toString());
+                bundle.putInt("a", a);
+                bundle.putInt("b", b);
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("mybackage", bundle);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Lab_07/Lab_07_02/LinearEquationApp/app/src/main/java/com/example/linearequationapp/ResultActivity.java
@@ -0,0 +1,55 @@
+package com.example.linearequationapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
+public class ResultActivity extends AppCompatActivity {
+    TextView txtResult;
+    Button btnBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_result);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        txtResult = findViewById(R.id.txt_result);
+        Intent myintent = getIntent();
+        Bundle myBundle = myintent.getBundleExtra("mybackage");
+        int a = myBundle.getInt("a");
+        int b = myBundle.getInt("b");
+        String result = "";
+        if(a==0 && b==0){
+            result = "Vô số nghiệm";
+        }
+        else if(a==0 && b!= 0){
+            result = "Vô nghiệm";
+        }
+        else{
+            DecimalFormat dcf = new DecimalFormat("0.##");
+            result = dcf.format(-b*1.0/a);
+        }
+        txtResult.setText(result+"");
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}

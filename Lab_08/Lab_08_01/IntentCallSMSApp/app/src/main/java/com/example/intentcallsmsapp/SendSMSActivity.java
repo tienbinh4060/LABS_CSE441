a9f839c3ea79213217bbec9220bc193cbac887eb
tienbinh4060 <tienbinhnguyen0246@gmail.com>
2025-06-06 03:01:36 +0700

    Them Lab_08_01 va Lab_08_02

---
+++ Lab_08/Lab_08_01/IntentCallSMSApp/app/src/main/java/com/example/intentcallsmsapp/SendSMSActivity.java
@@ -0,0 +1,49 @@
+package com.example.intentcallsmsapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SendSMSActivity extends AppCompatActivity {
+    Button btnBack;
+    ImageButton ibtnSMS;
+    EditText edtSMS;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_send_smsactivity);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        ibtnSMS = findViewById(R.id.ibtn_sms);
+        edtSMS = findViewById(R.id.edt_sms);
+        btnBack = findViewById(R.id.btn_back);
+        ibtnSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:"+edtSMS.getText().toString())));
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
